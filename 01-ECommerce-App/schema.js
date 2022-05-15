57cca5e121320ccc8fa68dab942af15102951cef
ainneo <ainneo@gmail.com>
2022-05-14 18:31:41 -0700

    01-ECommerce-App review & notes

--- 01-ECommerce-App/schema.js
+++ 01-ECommerce-App/schema.js
@@ -2,15 +2,30 @@ const { gql } = require("apollo-server");
 
 exports.typeDefs = gql`
   type Query {
+    #hello, products, product, categories, category are all feilds we can query...
+    #hello retubs a string
     hello: String
+    # query by ProductFilterInput from the list of Products
     products(filter: ProductsFilterInput): [Product!]!
+    # query by id in Product
     product(id: ID!): Product
+    # query a list of categories
     categories: [Category!]!
+    # query by id in Category
     category(id: ID!): Category
   }
 
+  #muating data
+  #UPDATE, DELETE, CREATE!!!!!!
+  #you CRUD in the GQL PG
+  #run and add new name "ainne"
+  #mutation{
+  #addCategory(input:{name: "ainne"} ) {
+  #  name
+  # }
+  #}
   type Mutation {
-    addCategory(input: AddCategoryInput!): Category!
+    addCategory(input: AddCategoryInput!): Category! #should return a name
     addProduct(input: AddProductInput!): Product!
     addReview(input: AddReviewInput!): Review!
     deleteCategory(id: ID!): Boolean!
@@ -51,7 +66,7 @@ exports.typeDefs = gql`
     onSale: Boolean
     avgRating: Int
   }
-
+  #passing this in our first mutation
   input AddCategoryInput {
     name: String!
   }
